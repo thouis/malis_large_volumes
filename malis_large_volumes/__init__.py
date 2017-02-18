df424f1496505b3bca9cb19908e16f44d6e7e334
Maltimore <malte.esders@gmail.com>
2017-02-18 22:23:42 +0100

    made separate python and cython versions to compare output

--- malis_large_volumes/__init__.py
+++ malis_large_volumes/__init__.py
@@ -1,9 +1,10 @@
 import numpy as np
 #from .malis_cython import build_tree
 # DEBUG should later be malis_cython
-from .malis_python import compute_pairs, build_tree
+from . import malis_python
+from . import malis_cython
 
-def pairs(labels, edge_weights, neighborhood=None):
+def pairs_python(labels, edge_weights, neighborhood=None):
     """
     This function simply combines the build_tree and compute_pairs functions
     """
@@ -13,6 +14,19 @@ def pairs(labels, edge_weights, neighborhood=None):
         neighborhood = np.array([[1, 0, 0],
                                  [0, 1, 0],
                                  [0, 0, 1]], dtype=np.int32)
-    edge_tree = build_tree(labels, edge_weights, neighborhood)
-    return compute_pairs(labels, edge_weights, neighborhood, edge_tree)
+    edge_tree = malis_python.build_tree(labels, edge_weights, neighborhood)
+    return malis_python.compute_pairs(labels, edge_weights, neighborhood, edge_tree)
 
+
+def pairs_cython(labels, edge_weights, neighborhood=None):
+    """
+    This function simply combines the build_tree and compute_pairs functions
+    """
+
+    if neighborhood is None:
+        print("No neighboorhood provided, using 3d NN neighboorhood")
+        neighborhood = np.array([[1, 0, 0],
+                                 [0, 1, 0],
+                                 [0, 0, 1]], dtype=np.int32)
+    edge_tree = malis_cython.build_tree(labels, edge_weights, neighborhood)
+    return malis_cython.compute_pairs(labels, edge_weights, neighborhood, edge_tree)
--- test/test_simple.py
+++ test/test_simple.py
@@ -1,11 +1,11 @@
 import numpy as np
-from malis_large_volumes import pairs
+from malis_large_volumes import pairs_python, pairs_cython
 
 labels = np.array([[[1, 1, 1, 2, 2, 2]]], dtype=np.int32)
 weights = np.zeros(shape=labels.shape + (3,))
 weights[:, :, :, 0] = np.array([[[.2, .2, .3, .2, .2, .2]]], dtype=np.float)
 
-pos_pairs, neg_pairs = pairs(labels, weights)
+pos_pairs, neg_pairs = pairs_python(labels, weights)
 import pdb; pdb.set_trace()
 
 
