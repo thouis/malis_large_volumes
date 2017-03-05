9a366dfc841384b7a89764342f1b62391d81c9db
Maltimore <malte.esders@gmail.com>
2017-03-05 21:45:43 +0100

    made default neighborhood to point in negative direction

--- malis_large_volumes/__init__.py
+++ malis_large_volumes/__init__.py
@@ -11,9 +11,9 @@ def pairs_python(labels, edge_weights, neighborhood=None):
 
     if neighborhood is None:
         print("No neighboorhood provided, using 3d NN neighboorhood")
-        neighborhood = np.array([[1, 0, 0],
-                                 [0, 1, 0],
-                                 [0, 0, 1]], dtype=np.int32)
+        neighborhood = np.array([[-1, 0, 0],
+                                 [0, -1, 0],
+                                 [0, 0, -1]], dtype=np.int32)
     edge_tree = malis_python.build_tree(labels, edge_weights, neighborhood)
     return malis_python.compute_pairs(labels, edge_weights, neighborhood, edge_tree)
 
@@ -22,11 +22,12 @@ def pairs_cython(labels, edge_weights, neighborhood=None):
     """
     This function simply combines the build_tree and compute_pairs functions
     """
+    labels = labels.astype(np.int32)
 
     if neighborhood is None:
         print("No neighboorhood provided, using 3d NN neighboorhood")
-        neighborhood = np.array([[1, 0, 0],
-                                 [0, 1, 0],
-                                 [0, 0, 1]], dtype=np.int32)
+        neighborhood = np.array([[-1, 0, 0],
+                                 [0, -1, 0],
+                                 [0, 0, -1]], dtype=np.int32)
     edge_tree = malis_cython.build_tree(labels, edge_weights, neighborhood)
     return malis_cython.compute_pairs(labels, edge_weights, neighborhood, edge_tree)
