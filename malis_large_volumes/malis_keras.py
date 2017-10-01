48edc7ab01710acd8c50cbdac8710d3565688be7
maltimore <malte.esders@gmail.com>
2017-10-01 18:39:45 +0200

    malis_keras: removed outcommented code and some linting

--- malis_large_volumes/malis_keras.py
+++ malis_large_volumes/malis_keras.py
@@ -33,18 +33,13 @@ class Malis:
         edges_not_in_lower_margin = tf.where(pred > self.margin, ones_helpervar, zeros_helpervar)
         loss_mask = tf.where(pos_pairs > neg_pairs, edges_not_in_upper_margin, edges_not_in_lower_margin)
 
-        # set pos or neg counts to zero depending on which are smaller
-#        pos_pairs = tf.where(pos_pairs > neg_pairs, ones_helpervar, zeros_helpervar)
-#        neg_pairs = tf.where(neg_pairs > pos_pairs, ones_helpervar, zeros_helpervar)
-
-        pos_loss = (1-pred)**2 * pos_pairs * loss_mask * self.pos_loss_weight
+        pos_loss = (1 - pred)**2 * pos_pairs * loss_mask * self.pos_loss_weight
         neg_loss = pred**2 * neg_pairs * loss_mask * self.neg_loss_weight
         # get the total loss at each element
         elemwise_total_loss = pos_loss + neg_loss
         malis_loss = K.sum(elemwise_total_loss, axis=(1, 2, 3, 4))
-        malis_loss = malis_loss * 2 # because of the pos_loss_weight and neg_loss_weight
+        malis_loss = malis_loss * 2  # because of the pos_loss_weight and neg_loss_weight
         return malis_loss
-
 
     def pairs_to_loss_python(self, pairs, pred):
         """
@@ -62,6 +57,7 @@ class Malis:
         neg_loss, vector of shape (batch_size,)
         """
         # the code is deliberately kept very similar to pairs_to_loss_keras
+        # in order to verify that the keras version does the right thing
         # (more performant code may very well be possile but that is not the goal)
 
         pos_pairs = pairs[:, 0:3]
@@ -74,11 +70,7 @@ class Malis:
         edges_not_in_lower_margin = np.where(pred > self.margin, ones_helpervar, zeros_helpervar)
         loss_mask = np.where(pos_pairs > neg_pairs, edges_not_in_upper_margin, edges_not_in_lower_margin)
 
-        # set pos or neg counts to zero depending on which are smaller
-#        pos_pairs = np.where(pos_pairs > neg_pairs, ones_helpervar, zeros_helpervar)
-#        neg_pairs = np.where(neg_pairs > pos_pairs, ones_helpervar, zeros_helpervar)
-
-        pos_loss = (1-pred)**2 * pos_pairs * loss_mask * self.pos_loss_weight
+        pos_loss = (1 - pred)**2 * pos_pairs * loss_mask * self.pos_loss_weight
         neg_loss = pred**2 * neg_pairs * loss_mask * self.neg_loss_weight
         # get the total loss at each element
         elemwise_total_loss = pos_loss + neg_loss
