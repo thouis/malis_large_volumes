d70552ad1988b4871222cc7e719c5d75bdf9577e
maltimore <malte.esders@gmail.com>
2017-10-02 14:37:55 -0400

    took out unnecessary code from recursive function

--- malis_large_volumes/recursive_tree_traversal.py
+++ malis_large_volumes/recursive_tree_traversal.py
@@ -10,9 +10,11 @@ import numpy as np
 
 def compute_pairs_recursive(labels, edge_weights, neighborhood, edge_tree, edge_tree_idx, pos_pairs, neg_pairs):
 
-    linear_edge_index, child_1, child_2 = edge_tree[edge_tree_idx, ...]
+    # extract current edge and its children
+    linear_edge_index, child_1, child_2 = edge_tree[edge_tree_idx, :]
     k, d_1, w_1, h_1 = np.unravel_index(linear_edge_index, edge_weights.shape)
-    assert linear_edge_index != -1  # also marks visited nodes
+
+    # CHILD 1
     if child_1 == -1:
         # first child is a voxel.  Compute its location
         region_counts_1 = {labels[d_1, w_1, h_1]: 1}
@@ -21,9 +23,10 @@ def compute_pairs_recursive(labels, edge_weights, neighborhood, edge_tree, edge_
         region_counts_1 = compute_pairs_recursive(labels, edge_weights, neighborhood,
                                                   edge_tree, child_1, pos_pairs, neg_pairs)
 
+    # CHILD 2
     if child_2 == -1:
         # second child is a voxel.  Compute its location via neighborhood.
-        offset = neighborhood[k, ...]
+        offset = neighborhood[k, :]
         d_2, w_2, h_2 = (o + d for o, d in zip(offset, (d_1, w_1, h_1)))
         region_counts_2 = {labels[d_2, w_2, h_2]: 1}
     else:
@@ -31,9 +34,7 @@ def compute_pairs_recursive(labels, edge_weights, neighborhood, edge_tree, edge_
         region_counts_2 = compute_pairs_recursive(labels, edge_weights, neighborhood,
                                                   edge_tree, child_2, pos_pairs, neg_pairs)
 
-    # mark this edge as done so recursion doesn't hit it again
-    edge_tree[edge_tree_idx, 0] = -1
-
+    # compute MALIS counts
     return_dict = {}
     for key1, counts1 in region_counts_1.items():
         for key2, counts2 in region_counts_2.items():
