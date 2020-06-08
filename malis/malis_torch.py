86702e2fe64b2910be42124320d572721c52cca3
HAICU Ruolin Shen <ruolin.shen@heidi.scidom.de>
2020-06-08 15:22:56 +0200

    modify malis loss 2d and 3d version

--- malis/malis_torch.py
+++ malis/malis_torch.py
@@ -6,11 +6,11 @@ import numpy as np
 
 class torchloss(Function):
     @staticmethod
-    def forward(ctx, aff_pred, seg_gt): #(b,2, 512,512) (b,1,512,512)
+    def forward(ctx, aff_pred, seg_gt,nhood=None): #(b,2, 512,512) (b,1,512,512)
         aff_pred = aff_pred.detach().numpy()
         seg_gt = seg_gt.detach().numpy()       
         
-        weights_pos,weights_neg = get_pairs(seg_gt,aff_pred)
+        weights_pos,weights_neg = get_pairs(seg_gt,aff_pred,nhood)
         weights_pos = weights_pos.astype(np.float32)
         weights_neg = weights_neg.astype(np.float32)
 
@@ -18,7 +18,7 @@ class torchloss(Function):
 
     @staticmethod
     def backward(ctx, grad_output1,grad_output2):
-        return None,None
+        return None,None,None
 
 def pairs_to_loss_torch(pos_pairs, neg_pairs, output, margin=0.3, pos_loss_weight=0.3):
     '''
@@ -46,24 +46,48 @@ def pairs_to_loss_torch(pos_pairs, neg_pairs, output, margin=0.3, pos_loss_weigh
     
     return loss
 
-def malis_loss(seg_gt,output): #(b,1,512,512) (b,3,512,512)
-    '''
-    input - output, Tensor(batch size, channel, H, W): predicted affinity graphs from network
-    input - seg_gt, Tensor(batch size, channel=1, H, W): segmentation groundtruth 
+def malis_loss2d(seg_gt,output): 
     
-    output - loss, Tensor(scale): malis loss 
-    '''
+    # Input:
+    #    output: Tensor(batch size, channel=2, H, W)
+    #           predicted affinity graphs from network
+    #    seg_gt: Tensor(batch size, channel=1, H, W)
+                segmentation groundtruth     
+    # Returns: 
+    #    loss: Tensor(scale)
+    #           malis loss 
     
+    ######### please modify here to make sure seg_gt and output has the correct shape
     x,y = seg_gt.shape[2],seg_gt.shape[3]
-    output.permute(1,2,3,0)  # (C=3,H,W,batch_size)
+    output = output.permute(1,2,3,0)           # (2,H,W,batch_size)
     seg_gt = seg_gt.reshape(x,y,-1)            # (H,W,C'=C*batch_size)
-        
-    pos_pairs,neg_pairs = torchloss.apply(output, seg_gt)
+    #########
     
-    pos_pairs.permute(-1,0,1,2)
-    neg_pairs.permute(-1,0,1,2)
-
+    nhood = malis.mknhood3d(1)[:-1]  
+    pos_pairs,neg_pairs = torchloss.apply(output, seg_gt, nhood)
     loss = pairs_to_loss_torch(pos_pairs, neg_pairs, output)
     
     return loss
 
+def malis_loss3d(seg_gt,output): 
+    
+    # Input:
+    #    output: Tensor(batch size=1, channel=3, H, W, D)
+    #           predicted affinity graphs from network
+    #    seg_gt: Tensor(batch size=1, channel=1, H, W, D)
+                segmentation groundtruth     
+    # Returns: 
+    #    loss: Tensor(scale)
+    #           malis loss 
+    
+    ######### please modify here to make sure seg_gt and output has the correct shape
+    x,y,z = seg_gt.shape[2],seg_gt.shape[3],seg_gt.shape[4]
+    output = output.reshape(-1,x,y,z)         # (3,H,W,D)
+    seg_gt = seg_gt.reshape(x,y,z)            # (H,W,D)
+    #########
+    
+    nhood = malis.mknhood3d(1) 
+    pos_pairs,neg_pairs = torchloss.apply(output, seg_gt, nhood)
+    loss = pairs_to_loss_torch(pos_pairs, neg_pairs, output)
+    
+    return loss
